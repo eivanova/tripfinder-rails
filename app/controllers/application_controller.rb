e793256de50031cd85f7fd6b0f9fa9f440090200
Elena Ivanova <eoivanova@abv.bg>
2015-10-25 00:59:41 +0300

    Use browser to determine locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,13 +9,18 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def set_locale
-    logger.error params[:locale]
-    I18n.locale = params[:locale] || I18n.default_locale
+    logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+    I18n.locale = extract_locale_from_accept_language_header || I18n.default_locale
+    logger.debug "* Locale set to '#{I18n.locale}'"
   end
 
   private
 
   def not_authenticated
     redirect_to login_path, alert: "Please login first"
+  end
+
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
   end
 end
