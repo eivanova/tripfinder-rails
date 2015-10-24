f965faff2c5734f4a7c4b1e9191a792261de64de
Elena Ivanova <eoivanova@abv.bg>
2015-10-25 00:33:36 +0300

    Avoid generation of assets, helpers and tests

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,12 @@ module TripfinderRails
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.generators do |g|
+       g.assets = false
+       g.helper = false
+       g.view_specs = false
+       g.test_framework = nil
+    end
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
